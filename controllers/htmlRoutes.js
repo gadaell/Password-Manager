b50eda59e12e77876fa7054998c54c20a2244a3e
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-16 19:42:41 -0500

    homeroutes starting to work

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,21 +1,20 @@
 const router = require("express").Router();
 const path = require("path");
 
-
 router.get("/dashboard", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../dashboard.html"));
-  });
+	res.render("dashboard");
+});
 
-  router.get("/password:id", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../password.html"));
-  });
+router.get("/password:id", (req, res) => {
+	res.sendFile(path.join(__dirname, "../../password.html"));
+});
 
-  router.get("/signup", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../signup.html"));
-  });
+router.get("/signup", (req, res) => {
+	res.render("signup");
+});
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../index.html"));
-  });
-  
-  module.exports = router;
+	res.render("login");
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const apiRoutes = require("./apiRoutes");
 const htmlRoutes = require("./htmlRoutes");
 const userRoutes = require("./user-routes");
 
-router.use('/', userRoutes);
-router.use('/', htmlRoutes);
-router.use('/', apiRoutes);
+router.use("/user", userRoutes);
+router.use("/", htmlRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
