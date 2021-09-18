8bdbd0101aa185734f51d61b5dd5b91be66c8432
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-09-18 05:33:36 -0500

    login is directed to dashboard once button is clicked

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -2,19 +2,19 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/dashboard", (req, res) => {
-	res.render("dashboard");
+  res.render("dashboard");
 });
 
 router.get("/password:id", (req, res) => {
-	res.sendFile(path.join(__dirname, "../../password.html"));
+  res.sendFile(path.join(__dirname, "../../password.html"));
 });
 
 router.get("/signup", (req, res) => {
-	res.render("signup");
+  res.render("signup");
 });
 
 router.get("/", (req, res) => {
-	res.render("login");
+  res.render("login");
 });
 
 module.exports = router;
