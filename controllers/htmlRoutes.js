9f9527a8ba1a063e1b9db3edd3a0a5e9760b2d1c
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-18 12:17:40 -0500

    auth from login working. need to creat a logout path

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -2,7 +2,13 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/dashboard", (req, res) => {
-	res.render("dashboard");
+	req.session.loggedIn = true;
+	console.log(req.session);
+	if (req.session.loggedIn) {
+		res.render("dashboard");
+	} else {
+		res.redirect("/");
+	}
 });
 
 router.get("/password:id", (req, res) => {
