74ed35cf7a053fca36550df3bc782c6073346cb7
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-19 13:52:36 -0500

    password modle user_id changed to link. please rebuild db to get working

--- controllers/password-routes.js
+++ controllers/password-routes.js
@@ -5,12 +5,14 @@ const CryptoJS = require("crypto-js");
 router.get("/passwords/user/:userid", (req, res) => {
 	Passwords.findAll({
 		where: {
-			user_id: req.params.userid,
+			link: req.params.userid,
 		},
 	})
 		.then((dbPassData) => {
 			if (!dbPassData) {
-				res.status(404).json({ message: "No password data found for this user" });
+				res
+					.status(404)
+					.json({ message: "No password data found for this user" });
 				return;
 			}
 			res.json(dbPassData);
@@ -22,60 +24,65 @@ router.get("/passwords/user/:userid", (req, res) => {
 });
 
 router.get("/passwords/:id", (req, res) => {
-Passwords.findOne({
-	where: {
-		id: req.params.id,
-	},
-})
-	.then((dbPassData) => {
-		if (!dbPassData) {
-			res.status(404).json({ message: "No password data found with this id" });
-			return;
-		}
-		var bytes  = CryptoJS.AES.decrypt(dbPassData.website_password, 'encryptMe');
-		dbPassData.website_password = bytes.toString(CryptoJS.enc.Utf8);
-		res.json(dbPassData);
+	Passwords.findOne({
+		where: {
+			id: req.params.id,
+		},
 	})
-	.catch((err) => {
-		console.log(err);
-		res.status(500).json(err);
-	});
+		.then((dbPassData) => {
+			if (!dbPassData) {
+				res
+					.status(404)
+					.json({ message: "No password data found with this id" });
+				return;
+			}
+			var bytes = CryptoJS.AES.decrypt(
+				dbPassData.website_password,
+				"encryptMe"
+			);
+			dbPassData.website_password = bytes.toString(CryptoJS.enc.Utf8);
+			res.json(dbPassData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 router.post("/passwords", (req, res) => {
 	console.log(req);
 	console.log(req.session);
-Passwords.create({
-	user_id:'3',
-	website: req.body.website,
-	website_username: req.body.website_username,
-	website_password: req.body.website_password,
-})
-	.then((dbPassData) => res.render("dashboard"))
-	.catch((err) => {
-		console.log(err);
-		res.status(500).json(err);
-	});
+	Passwords.create({
+		link: req.session.user_id,
+		website: req.body.website,
+		website_username: req.body.website_username,
+		website_password: req.body.website_password,
+	})
+		.then((dbPassData) => res.render("dashboard"))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 router.put("/passwords/:id", (req, res) => {
-Passwords.update(req.body, {
-	individualHooks: true,
-	where: {
-		id: req.params.id,
-	},
-})
-	.then((dbPassData) => {
-		if (!dbPassData[0]) {
-			res.status(404).json({ message: "No password found with this id" });
-			return;
-		}
-		res.json(dbPassData);
+	Passwords.update(req.body, {
+		individualHooks: true,
+		where: {
+			id: req.params.id,
+		},
 	})
-	.catch((err) => {
-		console.log(err);
-		res.status(500).json(err);
-	});
+		.then((dbPassData) => {
+			if (!dbPassData[0]) {
+				res.status(404).json({ message: "No password found with this id" });
+				return;
+			}
+			res.json(dbPassData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 router.delete("/passwords/:id", (req, res) => {
