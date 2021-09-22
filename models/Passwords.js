b9ff3abb25ceb098898f72c2a1ac9c49cdabdad7
Patrick Abernathy <pat.abs@gmail.com>
2021-09-21 19:53:59 -0500

    fixed Password model error and added session.destroy

--- models/Passwords.js
+++ models/Passwords.js
@@ -1,58 +1,57 @@
-const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
-const sequelize = require("../config/connection");
-var CryptoJS = require("crypto-js");
-class Passwords extends Model {}
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const CryptoJS = require("crypto-js");
+class Passwords extends Model {
+
+}
 
 Passwords.init(
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true,
-		},
-		link: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-		website: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		website_username: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		website_password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-	},
-	{
-		hooks: {
-			async beforeCreate(newPasswordData) {
-				newPasswordData.website_password = CryptoJS.AES.encrypt(
-					newPasswordData.website_password,
-					"encryptMe"
-				).toString();
-				return newPasswordData;
-			},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    link: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: false,
 
-			async beforeUpdate(updatedPasswordData) {
-				updatedPasswordData.website_password = CryptoJS.AES.encrypt(
-					updatedPasswordData.website_password,
-					"encryptMe"
-				).toString();
-				return updatedPasswordData;
-			},
-		},
-		sequelize,
-		timestamps: false,
-		freezeTableName: true,
-		underscored: true,
-		modelName: "passwords",
-	}
+    },
+    website_username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+ 
+      },
+    website_password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    }
+  },
+  {
+    hooks: {
+      async beforeCreate(newPasswordData) {
+       newPasswordData.website_password = CryptoJS.AES.encrypt(newPasswordData.website_password, 'encryptMe').toString();
+        return newPasswordData;
+      },
+
+      async beforeUpdate(updatedPasswordData) {
+        updatedPasswordData.website_password = CryptoJS.AES.encrypt(updatedPasswordData.website_password, 'encryptMe').toString();
+        return updatedPasswordData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'passwords'
+  }
 );
 
 module.exports = Passwords;
