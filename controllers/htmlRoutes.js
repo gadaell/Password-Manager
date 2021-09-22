f8f8c6997a9a82ab80ac1f531e43f91369925b14
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-21 21:14:16 -0500

    back button working. also started the changing of the dash view. still need to figure out the edit

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -26,19 +26,6 @@ router.get("/dashboard", (req, res) => {
 			console.log(err);
 		});
 });
-// const allUserPosts = {
-//   id: 1,
-//   link: 2,
-//   website: "google.com",
-//   website_username: "danielzk",
-//   website_password:
-//     "$2b$10$VOzpw3gyHM8wsGiNX9D7ze43XJ9NxqshuFIibOszvDmtP8tSz0WVu",
-// };
-
-//     res.render("dashboard", data);
-//   } else {
-//     res.redirect("/");
-//   }
 
 router.get("/password:id", (req, res) => {
 	res.sendFile(path.join(__dirname, "../../password.html"));
