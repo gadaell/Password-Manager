dcb720497fc157acd6253938c999121eedbbc0db
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-23 19:49:59 -0500

    fixed favicon

--- public/assets/javascript/edit-post.js
+++ public/assets/javascript/edit-post.js
@@ -1,28 +1,33 @@
 async function editFormHandler(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  const title = document.getElementById("website-edit").value
-  const password = document.getElementById("edit-password").value
-  const username = document.getElementById("edit-username").value
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ]; 
-  console.log(title);
-  const response = await fetch(`/api/passwords/${id}`, {
-    method: 'PUT',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-    website: title,
-    website_password: password,
-    website_username: username})
-    
-  });
+	const title = document.getElementById("website-edit").value;
+	const password = document.getElementById("edit-password").value;
+	const username = document.getElementById("edit-username").value;
+	const id = window.location.toString().split("/")[
+		window.location.toString().split("/").length - 1
+	];
+	console.log(title);
+	const response = await fetch(`/api/passwords/${id}`, {
+		method: "PUT",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			website: title,
+			website_password: password,
+			website_username: username,
+		}),
+	});
 
-  if (response.ok) {
-    window.location.replace("/dashboard")
-  } else {
-    alert(response.statusText);
-  }
+	if (response.ok) {
+		window.location.replace("/dashboard");
+	} else {
+		alert(response.statusText);
+	}
 }
 
-document.getElementById("dashboard-edit-submit").addEventListener('click', editFormHandler);
+document
+	.getElementById("dashboard-edit-submit")
+	.addEventListener("click", editFormHandler);
+document.getElementById("undo").addEventListener("click", function () {
+	window.location.replace("/dashboard");
+});
